ed89689e7db31e522435cd8d7e67eb945cc07ee3
Artem-Ky <artoad505@mail.ru>
2024-11-07 15:20:45 +0300

    fix: scroll fon popup in ios

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -35,21 +35,27 @@ export function Popup({ settings: { visible, content = {} }, setSettings }) {
 
   useEffect(() => {
     if (visible) {
-      document.body.style.overflow = 'hidden';
+      const scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = '100%';
+
+      document.body.dataset.scrollY = scrollY.toString();
       document.addEventListener('keydown', handleKeyDown);
     } else {
-      document.body.style.overflow = 'auto';
       document.removeEventListener('keydown', handleKeyDown);
+      const scrollY = document.body.dataset.scrollY;
+
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+
+      window.scrollTo(0, parseInt(scrollY || '0', 10));
     }
 
     if (popupScrollRef.current) {
       popupScrollRef.current.scrollTop = 0;
     }
-
-    return () => {
-      document.body.style.overflow = 'auto';
-      document.removeEventListener('keydown', handleKeyDown);
-    };
   }, [visible, handleKeyDown]);
 
   function togglePopup(e) {
